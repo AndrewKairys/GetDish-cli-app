7ec84a34bb7fe9380b8a107d11d81f4c14b9eaae
Andrew <andrewkairys@gmail.com>
2018-04-27 11:47:12 -0400

    refactored scraper to instantiate Dish objs, corrected CSS selectors

--- lib/get_dish/scraper.rb
+++ lib/get_dish/scraper.rb
@@ -4,19 +4,24 @@ class GetDish::Scraper
     @url = url
   end
 
-  def self.scrape_dishes
+  def scrape_dishes
     #scrape a dish based on user input of types: Meal, Snack, Treat, Drink.
     html = "https://ketodash.com/recipe"
     @doc = Nokogiri::HTML(open(html))
-    random_recipe = doc.css("div.card").text.split
-    random_recipe.each do |child|
-      binding.pry
-      name = doc.css("h5 a")[0].text.strip
-      calories = doc.css("li.list-group-item")[0].text.split.pop.to_i
-      protein = doc.css("li.list-group-item")[1].text.split.pop.to_i
-      fat = doc.css("li.list-group-item")[2].text.split.pop.to_i
-      net_carbs = doc.css("li.list-group-item")[3].text.split.pop.to_i
-      url = html + doc.css("h5 a").attr("href").text
+    #parse first Nokogiri @doc into more manageable nodes
+    card_doc = @doc.css("div.col-sm-6.col-lg-4")
+    card_doc.each do |child|
+      dish = GetDish::Dish.new
+      dish.type = child.css("span.badge.badge-info").text
+      dish.name = child.css("h5 a").text.strip
+      dish.calories = child.css("li.list-group-item")[0].text.split.pop.to_i
+      dish.protein = child.css("li.list-group-item")[1].text.split.pop.to_i
+      dish.fat = child.css("li.list-group-item")[2].text.split.pop.to_i
+      dish.net_carbs = child.css("li.list-group-item")[3].text.split.pop.to_i
+      dish.url = html + child.css("h5 a").attr("href").text
+
+      #binding.pry
+      dish.save
     end
   end
 
