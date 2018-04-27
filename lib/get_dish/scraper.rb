bdb39f3735d5d984bcc36fc688b3c36a4050324c
Andrew <andrewkairys@gmail.com>
2018-04-27 10:59:58 -0400

    refactoring, creating instance variable for Nokogiri Doc

--- lib/get_dish/scraper.rb
+++ lib/get_dish/scraper.rb
@@ -4,31 +4,20 @@ class GetDish::Scraper
     @url = url
   end
 
-  def self.scrape_site
-    @dish = []
-    random_dish = rand(1..10)
+  def self.scrape_dishes
+    #scrape a dish based on user input of types: Meal, Snack, Treat, Drink.
     html = "https://ketodash.com/recipe"
-    doc = Nokogiri::HTML(open(html))
-    random_recipe = doc.css("div.card")[random_dish].text.split
-    binding.pry
+    @doc = Nokogiri::HTML(open(html))
+    random_recipe = doc.css("div.card").text.split
     random_recipe.each do |child|
+      binding.pry
       name = doc.css("h5 a")[0].text.strip
       calories = doc.css("li.list-group-item")[0].text.split.pop.to_i
       protein = doc.css("li.list-group-item")[1].text.split.pop.to_i
       fat = doc.css("li.list-group-item")[2].text.split.pop.to_i
       net_carbs = doc.css("li.list-group-item")[3].text.split.pop.to_i
       url = html + doc.css("h5 a").attr("href").text
-
-      @dish.push(
-      name: name,
-      calories: calories,
-      protein: protein,
-      fat: fat,
-      net_carbs: net_carbs,
-      url: url)
-
-    @dish
-    #binding.pry
+    end
   end
 
 
