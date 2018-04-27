697ff35d0234205df8e139b6d45ce1f727d9d1e5
Andrew <andrewkairys@gmail.com>
2018-04-27 15:54:50 -0400

    added :type, created reader method for class container

--- lib/get_dish/dish.rb
+++ lib/get_dish/dish.rb
@@ -1,10 +1,14 @@
 class GetDish::Dish
 
   #Class Instance Variables
-  attr_accessor :name, :calories, :url, :protein, :fat, :net_carbs
+  attr_accessor :name, :calories, :url, :protein, :fat, :net_carbs, :type
 
   #Class Container
   @@all = []
+
+  def self.all
+    @@all
+  end
 
   #saves dish instances created by Scraper class
   def save
