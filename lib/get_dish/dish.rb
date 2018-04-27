eef22c5f9b866c96a401522cb8fecf28df0617f8
Andrew <andrewkairys@gmail.com>
2018-04-27 11:19:12 -0400

    Create class container, implement save method to store dish objects

--- lib/get_dish/dish.rb
+++ lib/get_dish/dish.rb
@@ -3,8 +3,12 @@ class GetDish::Dish
   #Class Instance Variables
   attr_accessor :name, :calories, :url, :protein, :fat, :net_carbs
 
+  #Class Container
+  @@all = []
 
-
-  
+  #saves dish instances created by Scraper class
+  def save
+    @@all << self
+  end
 
 end
