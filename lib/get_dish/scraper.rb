1634329cbed1bc80840363b6257b18594890c969
Andrew <andrewkairys@gmail.com>
2018-04-27 15:53:47 -0400

    needed to refactor so CSS selector returned all types

--- lib/get_dish/scraper.rb
+++ lib/get_dish/scraper.rb
@@ -12,7 +12,7 @@ class GetDish::Scraper
     card_doc = @doc.css("div.col-sm-6.col-lg-4")
     card_doc.each do |child|
       dish = GetDish::Dish.new
-      dish.type = child.css("span.badge.badge-info").text
+      dish.type = child.css("span.badge").text
       dish.name = child.css("h5 a").text.strip
       dish.calories = child.css("li.list-group-item")[0].text.split.pop.to_i
       dish.protein = child.css("li.list-group-item")[1].text.split.pop.to_i
