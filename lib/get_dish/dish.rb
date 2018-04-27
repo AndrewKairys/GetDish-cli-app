6ce5e0ff1863fb0b7884f3b523d4c7bf8ecd46db
Andrew <andrewkairys@gmail.com>
2018-04-27 10:49:07 -0400

    changing scrape site to scrape dishes and changing pattern

--- lib/get_dish/dish.rb
+++ lib/get_dish/dish.rb
@@ -1,36 +1,10 @@
 class GetDish::Dish
 
+  #Class Instance Variables
   attr_accessor :name, :calories, :url, :protein, :fat, :net_carbs
 
 
-  def self.today
-    #scrape Keto Dash:
-    #https://ketodash.com/recipe
 
-    self.scrape_site
-  end
-
-  def self.scrape_site
-    @dish = []
-    html = "https://ketodash.com/recipe"
-    doc = Nokogiri::HTML(open(html))
-      name = doc.css("h5 a")[0].text.strip
-      calories = doc.css("li.list-group-item")[0].text.split.pop.to_i
-      protein = doc.css("li.list-group-item")[1].text.split.pop.to_i
-      fat = doc.css("li.list-group-item")[2].text.split.pop.to_i
-      net_carbs = doc.css("li.list-group-item")[3].text.split.pop.to_i
-      url = html + doc.css("h5 a").attr("href").text
-
-      @dish.push(
-      name: name,
-      calories: calories,
-      protein: protein,
-      fat: fat,
-      net_carbs: net_carbs,
-      url: url)
-
-    @dish
-    #binding.pry
-  end
+  
 
 end
