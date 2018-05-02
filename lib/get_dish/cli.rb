0b4170be999b51261f464358db86a4a4c1a2f000
Andrew <andrewkairys@gmail.com>
2018-05-02 13:20:34 -0400

    updated CLI class

--- lib/get_dish/cli.rb
+++ lib/get_dish/cli.rb
@@ -1,38 +1,86 @@
 #CLI Controller
-
 class GetDish::CLI
 
   def call
-    list_dish
-    menu
-    goodbye
+    # Priming
+    GetDish::Scraper.new.scrape_dishes
+    puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    puts "Looking for something Keto friendly?"
+    engage
   end
 
-  def list_dish
-    @dish = GetDish::Dish.today
-    @dish.each_with_index(1) do |dish, i|
-      puts "#{i}. #{dish.name} - #{dish.calories} - #{dish.protein} - #{dish.fat}"
-    end
+  def engage
+    puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    puts <<~HEREDOC
+          Enter what type of Keto friendly
+          dish you'd like: Meal, Snack, Drink,
+          or Treat and you'll get a randomly
+          selected dish! Or select exit to
+          end the program.
+         HEREDOC
+    puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    input = gets.strip.downcase
+
+    dish = dish_by_type(input).sample
+
+    return_table(dish)
+
+    puts "Would you like to go to the recipe for this dish: y or n?"
+    puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    answer = gets.strip.downcase
+
+      if ["y"].include?(answer)
+        go_to_recipe(dish)
+      elsif ["n"].include?(answer)
+        engage
+      elsif ["exit"].include?(answer)
+        goodbye
+      else
+        puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+        puts "I don't understand that answer."
+        engage
+      end
   end
 
-  def menu
-    input = nil
-    while input != "exit"
-      puts "Enter dish number for more info, list, or exit:"
-     input = gets.strip.downcase
-
-     if input.to_i > 0
-       the_knife = @dish[input.to_i -1]
-     elsif input == "list"
-       list_deals
-     else
-       "Please type in: list or exit"
+  def dish_by_type(input)
+    case input
+     when "meal"
+        GetDish::Dish.all.select { |e| e.type == "Meal"}
+     when "snack"
+        GetDish::Dish.all.select { |e| e.type == "Snack"}
+     when "drink"
+        GetDish::Dish.all.select { |e| e.type == "Drink"}
+     when "treat"
+        GetDish::Dish.all.select { |e| e.type == "Treat"}
      end
-    end
-  end
+   end
 
+   def return_table(dish)
+       puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+       puts "#{dish.name}"
+       puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+       puts "Calories per serving:     #{dish.calories}"
+       puts "Fat:                      #{dish.fat}g"
+       puts "Protein:                  #{dish.protein}g"
+       puts "Net-Carbs:                #{dish.net_carbs}g"
+       puts "Recipe:                   #{dish.url}"
+       puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+   end
+
+   def go_to_recipe(dish)
+     system("xdg-open '#{dish.url}'")
+   end
+
+  # def list_all_dishes(type)
+  #   GetDish::Dish.all.each_with_index do |dish, i|
+  #     puts "#{i+1} #{dish.title}"
+  #   end
+  # end
+  #
   def goodbye
-    p "Stop by tomorrow for new dishes!"
+    puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    puts "Stop by tomorrow for more dishes!"
+    exit
   end
 
 end
