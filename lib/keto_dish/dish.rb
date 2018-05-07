53f95cc32c7a063f00fb984af8a7bf17912a3c57
howardbdev <howarddevennish@gmail.com>
2018-05-07 18:20:55 -0400

    add notes about how to prevent duplicate dishes from persisting

--- lib/keto_dish/dish.rb
+++ lib/keto_dish/dish.rb
@@ -1,29 +1,96 @@
-require "pry"
 class KetoDish::Dish
 
   #Class Instance Variables
   attr_accessor :name, :calories, :url, :protein, :fat, :net_carbs, :dish_type
 
+  #Class Container
+  @@all = []
+
   def initialize(attr_hash)
     # we're going to build dishes here
     # let's do it using mass assignment
     attr_hash.each { |attr_name, attr_value| self.send("#{attr_name}=", attr_value) }
-    @@all << self
+    # notice this #save call is what was missing near the end of our live coding session:
+    # (you were calling from the Scraper class but we can just call it here)
+    self.save
   end
 
-  #Class Container
-  @@all = []
 
   def self.all
     @@all
   end
 
+  def self.find_by_name(name)
+    all.find {|dish| dish.name == name}
+  end
+
   #saves dish instances created by Scraper class
   def save
+    # you could also add logic here to prevent duplicates from being persisted to @@all
     @@all << self
   end
 
-
-
 end
-Pry.start
+
+# Andrew, here is what I was talking about during our session.
+# This is a printout of my terminal after several iterations
+# of choosing various meals from the menu.  Notice that the same
+# recipe for Sesame Chicken is now in memory 6 times.  How can we
+# ensure this doesn't happen?
+
+=begin
+
+[2] pry(#<KetoDish::Scraper>)> KetoDish::Dish.all.select {|dish| dish.name == "Sesame Chicken"}
+=> [#<KetoDish::Dish:0x00007fb953ce2688
+  @calories=520,
+  @dish_type="Meal",
+  @fat=36,
+  @name="Sesame Chicken",
+  @net_carbs=4,
+  @protein=45,
+  @url="https://ketodash.com/recipe/sesame-chicken">,
+ #<KetoDish::Dish:0x00007fb953cd6a40
+  @calories=520,
+  @dish_type="Meal",
+  @fat=36,
+  @name="Sesame Chicken",
+  @net_carbs=4,
+  @protein=45,
+  @url="https://ketodash.com/recipe/sesame-chicken">,
+ #<KetoDish::Dish:0x00007fb957d43e20
+  @calories=520,
+  @dish_type="Meal",
+  @fat=36,
+  @name="Sesame Chicken",
+  @net_carbs=4,
+  @protein=45,
+  @url="https://ketodash.com/recipe/sesame-chicken">,
+ #<KetoDish::Dish:0x00007fb954052fc0
+  @calories=520,
+  @dish_type="Meal",
+  @fat=36,
+  @name="Sesame Chicken",
+  @net_carbs=4,
+  @protein=45,
+  @url="https://ketodash.com/recipe/sesame-chicken">,
+ #<KetoDish::Dish:0x00007fb9569a8d20
+  @calories=520,
+  @dish_type="Meal",
+  @fat=36,
+  @name="Sesame Chicken",
+[3] pry(#<KetoDish::Scraper>)> KetoDish::Dish.all.select {|dish| dish.name == "Sesame Chicken"}.count
+=> 6
+
+Once you've coded a solution, the output should look like this:
+[4] pry(#<KetoDish::Scraper>)> KetoDish::Dish.all.select {|dish| dish.name == "Sesame Chicken"}
+=> [#<KetoDish::Dish:0x00007fc90acb5d38
+  @calories=520,
+  @dish_type="Meal",
+  @fat=36,
+  @name="Sesame Chicken",
+  @net_carbs=4,
+  @protein=45,
+  @url="https://ketodash.com/recipe/sesame-chicken">]
+[5] pry(#<KetoDish::Scraper>)> KetoDish::Dish.all.select {|dish| dish.name == "Sesame Chicken"}.count
+=> 1
+=end
--- lib/keto_dish/scraper.rb
+++ lib/keto_dish/scraper.rb
@@ -20,11 +20,17 @@ class KetoDish::Scraper
       dish_attrs[:fat] = child.css("li.list-group-item")[2].text.split.pop.to_i
       dish_attrs[:net_carbs] = child.css("li.list-group-item")[3].text.split.pop.to_i
       dish_attrs[:url] = html + child.css("h5 a").attr("href").text.gsub("/recipe", "")
-
+      # hint: you could just add to the following line to prevent duplicates, using our
+      # self.find_by_name method built in Dish ... the advantage to putting the
+      # logic here is we don't create new Dish objects at all if there's already
+      # one that exists... we are assuming in this case that names are unique (which
+      # is probably fine in this case but won't always do it for us in other apps)
       KetoDish::Dish.new(dish_attrs)
       # binding.pry
+      # we don't need to call #save here anymore, as we're taking care of it in the Dish class
       # dish.save
     end
+    # binding.pry
   end
 
 end
